d41f2ca26b03564343e4630535e7a2620ed5ed6c
kaiqiang.cao <kaiqiang.cao@zhangmen.com>
2021-08-13 10:13:04 +0800

    feat: 调整cli

--- README.md
+++ README.md
@@ -1,19 +1,45 @@
-# sky-cli
-自定义脚手架
+### sky-cli
+自定义脚手架，命令sky，支持以下几个操作
 
-inquirer: 交互式命令
-child_process：开启子进程
-chalk：文字上色；
-funclib：工具库；
-commander：注册自定义命令
-ora：动态交互，例如loading
-fs：文件读写；
-
-
-通过npm安装之后，可以通过 sky 指令进行操作；
-
-并提供四个命令：
 add：添加模版，参数包括模版名称，git地址，分支；
 delete：删除模版，参数包括模版名字；
 init：初始化模版，参数包括模版名字；
 list：查看所有模版；
+
+### 常用的node插件
+
+* inquirer: 交互式命令；
+* child_process：开启子进程，执行各种命令；
+* chalk：文字上色；
+* funclib：工具库；
+* commander：注册自定义命令
+* ora：动态交互，例如loading
+* fs：文件读写；
+* fs-extra：文件操作工具集；
+* simple-git：git的基础操作；
+
+服务开发常用插件：
+
+* nodemon：监视源文件中任何的更改并自动重启服务器；
+* body-parser：处理程序之前，在中间件中对传入的请求体进行解析（response body）；
+* mongoose：在node.js异步环境下对mongodb进行便捷操作的对象模型工具；
+* koa（express）：服务端编程使用；
+
+### 项目源码目录结构如下：
+
+```
+├── README.md
+├── bin
+│   └── index.js
+├── commanders
+│   ├── add.js
+│   ├── custom.js
+│   ├── delete.js
+│   ├── init.js
+│   └── list.js
+├── package-lock.json
+├── package.json
+├── template.json
+└── utils
+    └── getCustomConfig.js
+```
--- commanders/init.js
+++ commanders/init.js
@@ -1,12 +1,54 @@
 const ora = require('ora');
 const template = require('../template');
 const inquirer = require('inquirer');
-const cp = require('child_process');
+const chalk = require('chalk');
+const path = require('path');
+const { spawn } = require('child_process');
+const git = require('simple-git');
+const fse = require('fs-extra');
 
 const defaultBranch = 'master';
 
+const removeRelationGit = async (catalogue) => {
+    try {
+        await fse.remove(path.resolve(catalogue, '\.git'));
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
+const getTemplate = (gitUrl, proName) => {
+    return new Promise((resolve, reject) => {
+        git().
+        //silent(true).
+        clone(gitUrl, proName).
+        then(() => {
+            console.log(chalk.green('template 拉取成功'));
+            resolve();
+        }).catch((err) => {
+            console.log(err);
+            reject(err);
+        });
+    });
+};
+
+const installDepences = (catalogue) => {
+    console.log(chalk.green('starting install dependencies'));
+    const inp = spawn(process.platform === 'win32' ? 'npm.cmd' : 'npm', ['install'], { cwd: catalogue, stdio: 'inherit' });
+
+    inp.on('close', (code) => {
+        if (code === 0) {
+            console.log(chalk.green('install dependencies success！'));
+            return;
+        }
+
+        console.log(chalk.red('install dependencies fail'));
+    })
+};
+
 const init = async (proName) => {
     try {
+        const filePath = path.resolve(process.cwd(), projectName);
         const tempList = Object.keys(template.tpl);
         const res = await inquirer.prompt({
             type: 'rawlist',
@@ -22,6 +64,12 @@ const init = async (proName) => {
         const proCmd = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${gitBranch}`;
         ora('开始初始化').start();
 
+        await fse.ensureDir(filePath);
+        await getTemplate(gitUrl, projectName);
+        await removeRelationGit(filePath);
+
+        process.exit();
+        /*
         cp.exec(proCmd, (err, stout, sterr) => {
             if (err) {
                 ora('\n 初始化失败！').fail(err.message);
@@ -32,9 +80,10 @@ const init = async (proName) => {
             ora('\n 初始化成功！').succeed();
             process.exit();
         });
+        */
     } catch (err) {
         ora('\n 初始化失败！').fail(err.message);
-        process.exit();
+        process.exit(1);
     }
 };
 
